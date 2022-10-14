654a163bd9389012370ffe7a9a0394f52b31b0b1
Qiao Yang Han <qyhwork@gmail.com>
2022-10-14 15:18:08 -0400

    Worked

--- movie_buff_exercises/movie_buff/02_queries.rb
+++ movie_buff_exercises/movie_buff/02_queries.rb
@@ -14,7 +14,7 @@ end
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-  
+  Actor.joins(:movies).where("movies.title = ?", title).order("castings.ord").select(:id, :name)
 end
 
 def vanity_projects
@@ -22,11 +22,16 @@ def vanity_projects
   # starring actor. Show the movie id, title, and director's name.
 
   # Note: Directors appear in the 'actors' table.
-  
+  Movie.joins(:actors).joins(:director).where("movies.director_id = actors.id AND castings.ord = 1").select("movies.id, movies.title, actors.name")
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name, and number of supporting roles.
-  
+  Actor.joins(:castings)
+  .where("castings.ord != 1")
+  .group("actors.id")
+  .order("COUNT(castings.id) DESC")
+  .limit(2)
+  .select("DISTINCT actors.id, actors.name, COUNT(castings.id) AS roles")
 end
